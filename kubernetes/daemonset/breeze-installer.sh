8cf59d6f818c1f8f82695ec626dec27322d310b9
zakatsv <afindir@cloudaware.com>
2021-10-06 19:18:53 +0300

    breeze ds reads host breeze agent logs

--- kubernetes/daemonset/breeze-installer.sh
+++ kubernetes/daemonset/breeze-installer.sh
@@ -4,13 +4,8 @@ sh /tmp/breeze-agent/install.sh
 
 while true
 do
-    # copy Breeze Agent files to the host machine
-    if [ ! -d "/opt/breeze-agent" ]
-    then
-        cp -R /tmp/breeze-agent /opt/breeze-agent
-        printf '%s: %s\n' "$(date)" "Breeze-agent folder has been created."
+    if [ -f "/var/log/breeze-agent.log" ]; then
+        tail -f /var/log/breeze-agent.log | while read line; do echo $line; done
     fi
-
-    printf '%s: %s\n' "$(date)" "Status: OK"
-    sleep 3600
+    sleep 300
 done
